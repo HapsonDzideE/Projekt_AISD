b55152b48f46695b59cdb2451692e102756d42b6
HapsonDzideE <szpilajakub@gmail.com>
2025-01-28 22:03:09 +0100

    Update Projekt_wersja_1.cpp
    Funkcja 1 - plus

--- Projekt_wersja_1.cpp
+++ Projekt_wersja_1.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 #include <vector>
-#icnlude <fstream>
+#include <fstream>
 
-
+// Funkcja do sprawdzania, czy dany punkt w tablicy tworzy prawidłowy "plus"
+bool czyPoprawnyPlus(const std::vector<std::vector<int>>& macierz, int w, int k) {
+    // Sprawdzenie czy centrum "plusa" jest jedynka
+    if (macierz[w][k] != 1) return false;
+    // Sprawdzanie czy ramiona "plusa" są jedynkami
+    if (macierz[w - 1][k] != 1 || macierz[w + 1][k] != 1 ||
+        macierz[w][k - 1] != 1 || macierz[w][k + 1] != 1) {
+        return false;
+    }
+}
 
 int main(){
 
