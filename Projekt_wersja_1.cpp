1b351fded61b2c711e0782a1f5d29f6bf8202a23
HapsonDzideE <szpilajakub@gmail.com>
2025-01-28 22:11:54 +0100

    Update Projekt_wersja_1.cpp
    Funkcja 1 - otoczenie plusa

--- Projekt_wersja_1.cpp
+++ Projekt_wersja_1.cpp
@@ -11,6 +11,20 @@ bool czyPoprawnyPlus(const std::vector<std::vector<int>>& macierz, int w, int k)
         macierz[w][k - 1] != 1 || macierz[w][k + 1] != 1) {
         return false;
     }
+    
+    // Sprawdzanie czy wokól "plusa" sa zera (lub granice tablicy)
+    if ((w - 2 >= 0 && macierz[w - 2][k] != 0) ||
+        (w + 2 < macierz.size() && macierz[w + 2][k] != 0) ||
+        (k - 2 >= 0 && macierz[w][k - 2] != 0) ||
+        (k + 2 < macierz[0].size() && macierz[w][k + 2] != 0) ||
+        (w - 1 >= 0 && k - 1 >= 0 && macierz[w - 1][k - 1] != 0) ||
+        (w - 1 >= 0 && k + 1 < macierz[0].size() && macierz[w - 1][k + 1] != 0) ||
+        (w + 1 < macierz.size() && k - 1 >= 0 && macierz[w + 1][k - 1] != 0) ||
+        (w + 1 < macierz.size() && k + 1 < macierz[0].size() && macierz[w + 1][k + 1] != 0)) {
+        return false;
+    }
+
+    return true;
 }
 
 int main(){
